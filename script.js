7c1d4a592d3ab7f191d86e7eb44b78076d6364f3
AndyCuba <zfesandydayona@gmail.com>
2020-09-09 21:41:10 +0300

    loops

---
+++ script.js
@@ -0,0 +1,30 @@
+
+//First variant
+
+
+/*for (i = 1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}*/
+
+
+//Second
+
+
+for (i = 1; i < 101; i++) {
+    let fizzBuzz = (i % 3 === 0 && i % 5 === 0) ? console.log('FizzBuzz'):
+    (i % 3 === 0) ? console.log('Fizz'):
+    (i % 5 === 0) ? console.log('Buzz'):
+    console.log(i);
+}
+
+
+
+
