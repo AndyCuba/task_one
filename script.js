1ab8f79bcc31330ec0a2a670ff992b085da1f85a
AndyCuba <zfesandydayona@gmail.com>
2020-09-10 16:05:09 +0300

    new tasks

--- script.js
+++ script.js
@@ -18,13 +18,40 @@
 //Second
 
 
-for (i = 1; i < 101; i++) {
+/*for (i = 1; i < 101; i++) {
     let fizzBuzz = (i % 3 === 0 && i % 5 === 0) ? console.log('FizzBuzz'):
     (i % 3 === 0) ? console.log('Fizz'):
     (i % 5 === 0) ? console.log('Buzz'):
     console.log(i);
-}
+}*/
 
 
+//Third task
 
 
+// let sentence = "Лето удалось очень жарким, было много ягод и фруктов, лучшим местом для отдыха была речка";
+
+// // console.log(sentence.replace(",", "."))
+// // sentence.split(',')
+// // console.log(sentence.indexOf(sentence))
+
+// for (let char of sentence) {
+//     if (char.includes (",")) {
+//         console.log(sentence.replace(",", "."))
+//     }
+// }
+
+// console.log(sentence);
+
+
+//Fourth task
+
+
+const sentence = "Антон, Дима и Женя пришли на День рождения сразу, а Миша пришел позже";
+let anton = sentence.substring(0,5);
+let dima = sentence.substring(7,11);
+let jenya = sentence.substring(14,18);
+let michael = sentence.substring(52,56);
+
+let newSentence = "На день рождения пришли: " + anton + ", " + dima + ", " + jenya + ", " + michael;
+console.log(newSentence);
